0eaebf86b1bbb0f33a5f3c68101125e016d3242d
Napoleon Izquieta <napoleon.izquieta@gmail.com>
2021-11-26 23:51:39 +0100

    :white_check_mark: Adding smoke test for App

--- App.test.tsx
+++ App.test.tsx
@@ -4,7 +4,7 @@ import { render } from "@testing-library/react-native";
 
 test("This is a test", () => {
   const screen = render(<App />);
-  const result = screen.getByText("Read the docs to discover what to do next:");
+  const result = screen.getByText("El xef xerraire");
 
   expect(result).not.toBe(null);
 });
