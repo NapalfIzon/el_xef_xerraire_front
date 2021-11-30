0e2c2bf2b8b50cc6ed4078be4c4cec5c07eb69f7
Napoleon Izquieta <napoleon.izquieta@gmail.com>
2021-11-30 23:20:00 +0100

    :memo: Modifying action interfaces

--- src/interfaces/actionsInterface.ts
+++ src/interfaces/actionsInterface.ts
@@ -1,10 +1,6 @@
 export interface IActionType {
   loadUser: string;
-  userLogin: string;
   userLogout: string;
-  modifyUser: string;
-  addFavorite: string;
-  removeFavorite: string;
   getRecipe: string;
   searchRecipes: string;
   addRecipe: string;
