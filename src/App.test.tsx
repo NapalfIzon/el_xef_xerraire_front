0a38ac20a93a2dfa952b6cc66ecfad90ad916ff4
Napoleon Izquieta <napoleon.izquieta@gmail.com>
2021-12-01 22:41:23 +0100

    Delete App.test.tsx

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import App from "./App";
-import React from "react";
-import { render } from "@testing-library/react-native";
-
-test("This is a test", () => {
-  render(<App />);
-
-  expect(true).not.toBe(false);
-});
