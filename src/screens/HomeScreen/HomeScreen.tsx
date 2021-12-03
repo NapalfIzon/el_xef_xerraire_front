3ccf88ff260df654c0dc04680dab1b3f6eb9e59c
Napoleon Izquieta <napoleon.izquieta@gmail.com>
2021-12-03 10:31:01 +0100

    :arrow_up: Adding Screens pages

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { SafeAreaView, ScrollView, View } from "react-native";
 import BackgroundImage from "../../components/BackgrounImage/BackgrounImage";
 import LastRecipes from "../../components/LastRecipes/LastRecipes";
-import Navbar from "../../components/Navbar/Navbar";
 import styles from "./HomeScreen.styles";
 
 const HomeScreen = () => {
@@ -10,7 +9,6 @@ const HomeScreen = () => {
     <SafeAreaView style={styles.body}>
       <ScrollView>
         <View>
-          <Navbar />
           <BackgroundImage />
           <LastRecipes />
         </View>
---
+++ src/screens/ProfileScreen/ProfileScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const ProfileScreen = () => {
+  return (
+    <View>
+      <Text>Yo seré la ProfileScreen</Text>
+    </View>
+  );
+};
+
+export default ProfileScreen;
