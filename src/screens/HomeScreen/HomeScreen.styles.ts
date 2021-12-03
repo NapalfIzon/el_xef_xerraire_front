861f3824a2c1bc92739887c613eeb575c73ccc97
Napoleon Izquieta <napoleon.izquieta@gmail.com>
2021-12-03 17:20:51 +0100

    :zap: improving HomeScreen

--- src/screens/HomeScreen/HomeScreen.styles.ts
+++ src/screens/HomeScreen/HomeScreen.styles.ts
@@ -4,4 +4,11 @@ export default StyleSheet.create({
   body: {
     flex: 1,
   },
+  backgroundImage: {
+    flex: 1,
+    justifyContent: "center",
+    height: 250,
+    width: 250,
+    opacity: 0.4,
+  },
 });
