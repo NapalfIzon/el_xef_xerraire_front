ea4d7e26d6d5ffcac01d4e0ce124954a5ac40b6f
Napoleon Izquieta <napoleon.izquieta@gmail.com>
2021-12-03 10:29:55 +0100

    :arrow_up: Adding Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,74 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { HStack, IconButton, Icon } from "native-base";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import {
+  faHome,
+  faSearch,
+  faPlusCircle,
+} from "@fortawesome/free-solid-svg-icons";
+import { faBookmark, faUserCircle } from "@fortawesome/free-regular-svg-icons";
 import styles from "./Navbar.styles";
 
 const Navbar = () => {
   return (
-    <View style={styles.temporal}>
-      <Text>Yo seré la Navbar</Text>
-    </View>
+    <>
+      <HStack
+        bg="#AB9B7A"
+        px="1"
+        py="3"
+        justifyContent="center"
+        justifyItems="center"
+        alignItems="center"
+      >
+        <HStack space="8" alignItems="center" paddingBottom="2">
+          <IconButton
+            icon={
+              <Icon
+                size="lg"
+                as={<FontAwesomeIcon icon={faHome} />}
+                color="white"
+              />
+            }
+          />
+          <IconButton
+            icon={
+              <Icon
+                as={<FontAwesomeIcon icon={faSearch} />}
+                size="xl"
+                color="white"
+              />
+            }
+          />
+          <IconButton
+            icon={
+              <Icon
+                as={<FontAwesomeIcon icon={faPlusCircle} />}
+                color="white"
+                size="sm"
+              />
+            }
+          />
+          <IconButton
+            icon={
+              <Icon
+                as={<FontAwesomeIcon icon={faBookmark} />}
+                size="sm"
+                color="green"
+              />
+            }
+          />
+          <IconButton
+            icon={
+              <Icon
+                as={<FontAwesomeIcon icon={faUserCircle} />}
+                size="sm"
+                color="white"
+              />
+            }
+          />
+        </HStack>
+      </HStack>
+    </>
   );
 };
 
