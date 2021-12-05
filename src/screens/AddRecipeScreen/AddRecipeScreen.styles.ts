b58c93336f845a049f5f0b714f952cc1a7faa067
Napoleon Izquieta <napoleon.izquieta@gmail.com>
2021-12-05 02:36:47 +0100

    :arrow_up: Adding AddRecipeScreen and RecipeForm component

---
+++ src/screens/AddRecipeScreen/AddRecipeScreen.styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  body: {
+    flex: 1,
+  },
+  backgroundImage: {
+    flex: 1,
+    justifyContent: "center",
+    height: 250,
+    width: 250,
+    opacity: 0.4,
+  },
+});
