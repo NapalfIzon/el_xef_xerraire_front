ac1b4ce5e76a07d38bac7ea7a35d7d41f8b923d5
Napoleon Izquieta <napoleon.izquieta@gmail.com>
2021-12-05 02:33:26 +0100

    :building_construction: Applying changes in configuration files

--- @types/dependencies_types.d.ts
+++ @types/dependencies_types.d.ts
@@ -1,3 +1,4 @@
 declare module "react-native-stars";
 declare module "react-native-vector-icons/MaterialCommunityIcons";
 declare module "react-native-vector-icons/FontAwesome";
+declare module "uuid";
--- env.d.ts
+++ env.d.ts
@@ -1,3 +1,4 @@
 declare module "@env" {
   export const API_XERRAPI_ENDPOINT: string;
+  export const TOKEN: string;
 }
