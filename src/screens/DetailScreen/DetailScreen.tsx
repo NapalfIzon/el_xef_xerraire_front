2b96b7152da9d8570d77a8c14a15948782ad9251
Napoleon Izquieta <napoleon.izquieta@gmail.com>
2021-12-06 20:03:21 +0100

    :arrow_up: Adding DetailScreen and RecipeDetail

--- src/screens/DetailScreen/DetailScreen.tsx
+++ src/screens/DetailScreen/DetailScreen.tsx
@@ -1,11 +1,27 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { ScrollView, SafeAreaView } from "react-native";
+import { NativeBaseProvider } from "native-base";
+import { NavigationRouteProps } from "../../types/propTypes";
+import styles from "./DetailScreen.styles";
+import Navbar from "../../components/Navbar/Navbar";
+import RecipeDetail from "../../components/RecipeDetail/RecipeDetail";
+import BackgroundImage from "../../components/BackgroundImage/BackgroundImage";
 
-const DetailScreen = () => {
+export const DetailScreen = ({ route, navigation }: NavigationRouteProps) => {
+  const {
+    params: { recipeData },
+  } = route;
+
   return (
-    <View>
-      <Text>Yo seré la DetailScreen</Text>
-    </View>
+    <SafeAreaView style={styles.body}>
+      <BackgroundImage />
+      <NativeBaseProvider>
+        <ScrollView>
+          <RecipeDetail navigation={navigation} recipeData={recipeData} />
+        </ScrollView>
+      </NativeBaseProvider>
+      <Navbar navigation={navigation} />
+    </SafeAreaView>
   );
 };
 
